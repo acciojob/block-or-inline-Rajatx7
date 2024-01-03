37f660fdd007cc84262333459cd2d826b985e8a5
Rajat Singh <155432801+Rajatx7@users.noreply.github.com>
2024-01-03 22:56:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your code here
+let elements = document.querySelectorAll("[data-ns-test]");
+
+elements.forEach((element) => {
+  let displayProperty = window.getComputedStyle(element).display;
+
+  if (displayProperty === "block") {
+    console.log("Block element");
+  } else if (displayProperty === "inline") {
+    console.log("Inline element");
+  }
+}
